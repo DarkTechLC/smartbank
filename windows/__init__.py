2acf29f1a188a97178a33ca1427a64a52fa07946
Luís Clício <cliciosa14@gmail.com>
2022-08-03 10:16:45 -0300

    feat: exports all window controllers

---
+++ windows/__init__.py
@@ -0,0 +1,6 @@
+from .login import LoginWindow
+from .register import RegisterWindow
+from .home import HomeWindow
+from .withdraw import WithdrawWindow
+from .deposit import DepositWindow
+from .transfer import TransferWindow
